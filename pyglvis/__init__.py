017bf51a6805ce643bafc4e2dd54d0763e144ece
Justin Laughlin <justinglaughlin@gmail.com>
2024-02-09 13:51:00 -0800

    changing glvis->pyglvis and class glvis to class Glvis

---
+++ pyglvis/__init__.py
@@ -0,0 +1,4 @@
+from pyglvis.__about__ import __version__
+from pyglvis.widget import Glvis
+
+__all__ = ["Glvis", "__version__"]
--- pyglvis/widget.py
+++ pyglvis/widget.py
@@ -4,7 +4,7 @@ from traitlets import Unicode, Int, Bool
 from pathlib import Path
 
 # Import file contents example: https://github.com/juba/pyobsplot/blob/main/src/pyobsplot/widget.py
-class glvis(anywidget.AnyWidget):
+class Glvis(anywidget.AnyWidget):
     _esm = anywidget._file_contents.FileContents(
         Path(__file__).parent / "widget.js", start_thread=False
     )
